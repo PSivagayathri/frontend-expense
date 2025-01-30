3a6dc73121e76c08cefb807629103e248f54178a
gayathri <shivanigayathri2006@gmail.com>
2025-01-30 09:42:50 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import './index.css';
 import AddTransaction from './components/AddTransaction';
@@ -8,25 +8,52 @@ import IncomeExpense from './components/IncomeExpense';
 
 function App() {
     const [transactions, setTransactions] = useState([
-        { id: 1, title: "laptop", amount: -60000 },
-        { id: 2, title: "salary", amount: 250000 },
-        { id: 3, title: "trip-vietnam", amount: -70000 },
-        { id: 4, title: "dress", amount: -1500 },
+        // { id: 1, title: "laptop", amount: -60000 },
+        // { id: 2, title: "salary", amount: 250000 },
+        // { id: 3, title: "trip-vietnam", amount: -70000 },
+        // { id: 4, title: "dress", amount: -1500 },
     ]);
+    useEffect(()=>{
+        fetch("http://localhost:8000/api/expenses")
+        .then(res=>res.json())  
+        .then(data=>setTransactions(data)) 
+    
+    },[])
 
     const onAddTransaction = (data) => {
-        const modifyData = { ...data, id: Math.random() * 1000 };
-        setTransactions([...transactions, modifyData]);
+        fetch("http://localhost:8000/api/expenses", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((newTransaction) => {
+                setTransactions([...transactions, newTransaction]);
+                console.log("content added to the db successfully")
+            })
+            .catch((error) => console.error("Error adding transaction:", error));
     };
 
     const onDeleteTransaction = (id) => {
-        const newTransactions = transactions.filter((eachTransaction) => eachTransaction.id !== id);
-        setTransactions(newTransactions);
+        fetch(`http://localhost:8000/api/expenses/${id}`, {
+            method: "DELETE",
+        })
+            .then((res) => {
+                if (res.ok) {
+                    setTransactions(transactions.filter((eachTransaction) => eachTransaction.id !== id));
+                } else {
+                    console.error("Error deleting transaction");
+                }
+            })
+            .catch((error) => console.error("Error deleting transaction:", error));
     };
+
 
     return (
         <>
-            <h1>Expense Tracker</h1>
+            <h1>EXPENSE-TRACKER</h1>
             <div className="container">
                 <Balance transactions={transactions} />
                 <IncomeExpense transactions={transactions} />
